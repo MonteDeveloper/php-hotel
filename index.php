b74b26d217e7b994604bb2d493a51b7ea74b415f
MonteDeveloper <lorenzomontello83@gmail.com>
2023-06-08 18:10:02 +0200

    bonus

--- index.php
+++ index.php
@@ -56,28 +56,63 @@
 
 <body class="bg-dark text-white">
     <div class="container p-3 pt-5">
-        <table class="table table-dark table-hover text-center">
-            <tr>
-                <th class="table-dark">Nome</th>
-                <th class="table-dark">Descrizione</th>
-                <th class="table-dark">Parcheggio</th>
-                <th class="table-dark">Valutazione</th>
-                <th class="table-dark">Distanza dal centro</th>
-            </tr>
-            <?php
-            foreach ($hotels as $hotel) {
-                echo '<tr>';
-                
-                echo '<td class="table-dark">' . $hotel['name'] . '</td>';
-                echo '<td class="table-dark">' . $hotel['description'] . '</td>';
-                echo $hotel['parking'] ? '<td class="table-dark">Sì</td>' : '<td class="table-dark">No</td>';
-                echo '<td class="table-dark">' . $hotel['vote'] . '</td>';
-                echo '<td class="table-dark">' . $hotel['distance_to_center'] . ' km</td>';
+        <div class="d-flex flex-column gap-4">
+            <h1 class="text-center text-decoration-underline">Trova il tuo Hotel!</h1>
+            <div class="shadow rounded border p-5">
+                <form action="index.php" method="GET">
+                    <h2>Filtri:</h2>
+                    <!-- VOTE -->
+                    <select class="mb-3 form-select" aria-label="Default select example" name="vote">
+                        <option value="">Nessun voto</option>
+                        <?php
+                            for($voto = 1; $voto <= 5; $voto++){
+                                $isSelected = $_GET["vote"] == $voto ? "selected" : "";
+                                echo $voto < 5 ? "<option value='$voto' $isSelected>$voto o più</option>" : "<option value='$voto' $isSelected>$voto</option>";
+                            }
+                        ?>
+                    </select>
 
-                echo '</tr>';
-            }
-            ?>
-        </table>
+                    <!-- PARKING -->
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="parkCheckbox" name="parking" <?php echo $_GET["parking"] == "on" ? "checked" : "" ?> >
+                        <label class="form-check-label" for="parkCheckbox">Disponibilità parcheggio</label>
+                    </div>
+
+                    <!-- BUTTONS -->
+                    <button type="submit" class="btn btn-success">Applica</button>
+                </form>
+            </div>
+
+            <div class="shadow rounded border p-5">
+                <h2>Hotel:</h2>
+                <table class="table table-dark table-hover text-center">
+                    <tr>
+                        <th class="table-dark">Nome</th>
+                        <th class="table-dark">Descrizione</th>
+                        <th class="table-dark">Parcheggio</th>
+                        <th class="table-dark">Valutazione</th>
+                        <th class="table-dark">Distanza dal centro</th>
+                    </tr>
+                    <?php
+                    $isParkingChecked = $_GET["parking"] == "on";
+
+                    foreach ($hotels as $hotel) {
+                        if($_GET["vote"] <= $hotel['vote'] && $isParkingChecked == $hotel['parking']){
+                            echo '<tr>';
+    
+                            echo '<td class="table-dark">' . $hotel['name'] . '</td>';
+                            echo '<td class="table-dark">' . $hotel['description'] . '</td>';
+                            echo $hotel['parking'] ? '<td class="table-dark">Sì</td>' : '<td class="table-dark">No</td>';
+                            echo '<td class="table-dark">' . $hotel['vote'] . '</td>';
+                            echo '<td class="table-dark">' . $hotel['distance_to_center'] . ' km</td>';
+    
+                            echo '</tr>';
+                        }
+                    }
+                    ?>
+                </table>
+            </div>
+        </div>
     </div>
 
     <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4' crossorigin='anonymous'></script>
